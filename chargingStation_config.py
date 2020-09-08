ce1b2a38a61bd5c2a6598e594e083cfda73bc3e5
jpieschacon <juaranga95hotmail.com>
2020-09-08 12:26:55 +0200

    Postponing the charging of a fraction of batteries if the cost is high and if there is NOT sunlight (numeral 3 laboratory). Several buf fixes (mostly time travel)

--- chargingStation_config.py
+++ chargingStation_config.py
@@ -1,32 +1,12 @@
 import random
 import json
-from queue import Queue, PriorityQueue
 import numpy as np
-import seaborn as sns
 from scipy.stats import t, sem
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 from tqdm import tqdm
 
-C = 40  # Max battery capacity
-NBSS = 10  # Max number of chargers
-Wmax = 20  # Max waiting time for EV
-Bth = 40  # Battery capacity
-BthHighDemand = 40  # Accepted minimum charge level
-deltaHighDemand = 60
-lossesHighDemand = 2
-chargingRate = 20  # charging rate per hour
-maxChargingRate = 20  # Fixed charging rate
-PV = 0  # Number of Photovoltaic Panels
-S_one_PV = 1  # Nominal Cap. of one PV (1kWp)
-prices = pd.read_csv('Data/electricity_prices.csv')  # Prices dataframe
-PV_production = pd.read_csv('Data/PVproduction_PanelSize1kWp.csv')  # Output PV power dataframe
-day = 15
-month = 11
-Spv = 0  # Nominal Cap. of the set of PV (kW), as we start at midnight the nom. cap. will always be 0
-Tmax = 60  # maximum time by which the charge process can be postponed (in minutes)
-f = 0.8  # postpone the charging of a fraction of batteries f
 
 class Measure:
     def __init__(self):
@@ -68,7 +48,7 @@ class Battery:
             # self.level = batLevel(0, 2)
             # TODO implement k method to remove warm-up
             # TODO graph for different initial charging values the warm-up period
-            self.level = batLevel(10, 1)
+            self.level = batLevel(20, 21)
             self.estimateAvailable = self.arrival_time + ((Bth - self.level) * 60 / chargingRate)  # estimated waiting time for next available battery
             FES.put((self.estimateAvailable, "batteryAvailable", charger))
         else:
@@ -347,49 +327,99 @@ def getLosses(lossses_list, time, delta):
 
 
 if __name__ == '__main__':
+    config = json.load(open('config.json'))
     random.seed(42)
     np.random.seed(42)
     SIM_TIME = 24 * 60  # Simulation time in minutes
-    time = 0
-    waitingLine = []
-    data = Measure()
-    # FES = PriorityQueue()  # list of events
-    FES = PriorityQueue_implemented()
-    FES.put((0, "arrival", -1))  # schedule first arrival at t=0
-    FES.put((60, "chargingRate_change", -1))
-    chargers = Charger(NBSS)
-    listTime = []
-    listChargingRate = []
-    pbar = tqdm(total=SIM_TIME)
-    while time < SIM_TIME:
-        (time, event_type, charger) = FES.get()
-        if time < data.oldT:
-            print('Time travel')
-        if event_type == "arrival":
-            arrival(time, FES, waitingLine)
+    C = 40  # Max battery capacity
+    NBSS = 10  # Max number of chargers
+    Wmax = 20  # Max waiting time for EV
+    Bth = 40  # Battery capacity
+    BthHighDemand = 40  # Accepted minimum charge level
+    deltaHighDemand = 60
+    lossesHighDemand = 2
+    chargingRate = 20  # charging rate per hour
+    maxChargingRate = 20  # Fixed charging rate
+    PV = 0  # Number of Photovoltaic Panels
+    S_one_PV = 1  # Nominal Cap. of one PV (1kWp)
+    prices = pd.read_csv('Data/electricity_prices.csv')  # Prices dataframe
+    PV_production = pd.read_csv('Data/PVproduction_PanelSize1kWp.csv')  # Output PV power dataframe
+    day = 15
+    month = 11
+    Spv = 0  # Nominal Cap. of the set of PV (kW), as we start at midnight the nom. cap. will always be 0
+    Tmax = 60  # maximum time by which the charge process can be postponed (in minutes)
+    f = 0.4  # postpone the charging of a fraction of batteries f
+    for key in config.keys():
+        PV = config[key]['PV']
+        f_vector = np.linspace(config[key]['f']['start'], config[key]['f']['finish'], config[key]['f']['samples'])
+        Tmax_vector = np.linspace(config[key]['Tmax']['start'], config[key]['Tmax']['finish'], config[key]['Tmax']['samples'])
+        for Tmax in Tmax_vector:
+            print(f"{key}_{Tmax}")
+            config[key]['Tmax'][f'Time_{Tmax}'] = []
+            config[key]['Tmax'][f'LossProb_{Tmax}'] = []
+            config[key]['Tmax'][f'Cost_{Tmax}'] = []
+            config[key]['Tmax'][f'Working_{Tmax}'] = []
+            time = 0
+            waitingLine = []
+            data = Measure()
+            # FES = PriorityQueue()  # list of events
+            FES = PriorityQueue_implemented()
+            FES.put((0, "arrival", -1))  # schedule first arrival at t=0
+            FES.put((60, "chargingRate_change", -1))
+            chargers = Charger(NBSS)
+            while time < SIM_TIME:
+                (time, event_type, charger) = FES.get()
+                if time < data.oldT:
+                    print('Time travel')
+                if event_type == "arrival":
+                    arrival(time, FES, waitingLine)
 
-        elif event_type == "batteryAvailable":
-            batteryAvailable(time, FES, waitingLine, charger)
+                elif event_type == "batteryAvailable":
+                    batteryAvailable(time, FES, waitingLine, charger)
 
-        elif event_type == "chargingRate_change":
-            chargingRate_change(time, FES)
+                elif event_type == "chargingRate_change":
+                    chargingRate_change(time, FES)
 
-        elif event_type == "reconnectBatteries":
-            reconnectBatteries(time, FES)
-        listTime.append(time)
-        listChargingRate.append(chargingRate)
-        data.lossProb.append(len(data.loss)/data.arr)
-        data.timelossProb.append(time)
-        data.timeCost.append(data.cost)
-        pbar.update(time)
-    confidence_int_wait = t.interval(0.999, len(data.waitingTime) - 1, np.mean(data.waitingTime), sem(data.waitingTime))
-    confidence_int_charge = t.interval(0.999, len(data.chargingTime) - 1, np.mean(data.chargingTime), sem(data.chargingTime))
-    # Loss warm-up period (hour 0-3)
-    warm_loss = np.count_nonzero(np.array(data.loss) < 180)
-    print(f"Confidence interval Waiting Time: {confidence_int_wait}")
-    print(f"Confidence interval Charging Time: {confidence_int_charge}")
-    print(f"Number of arrivals: {data.arr}")
-    print(f"Number of departures: {data.dep}")
-    print(f"Number of losses: {len(data.loss)}")
-    print(f"Number of losses in the warm-up interval: {warm_loss}")
+                elif event_type == "reconnectBatteries":
+                    reconnectBatteries(time, FES)
+                config[key]['Tmax'][f'Time_{Tmax}'].append(time)
+                config[key]['Tmax'][f'LossProb_{Tmax}'].append(len(data.loss) / data.arr)
+                config[key]['Tmax'][f'Cost_{Tmax}'].append(data.cost)
+                config[key]['Tmax'][f'Working_{Tmax}'].append([charger.working for charger in chargers.chargers].count(True))
+        for f in f_vector:
+            print(f"{key}_{f}")
+            config[key]['f'][f'Time_{f}'] = []
+            config[key]['f'][f'LossProb_{f}'] = []
+            config[key]['f'][f'Cost_{f}'] = []
+            config[key]['f'][f'Working_{f}'] = []
+            time = 0
+            waitingLine = []
+            data = Measure()
+            # FES = PriorityQueue()  # list of events
+            FES = PriorityQueue_implemented()
+            FES.put((0, "arrival", -1))  # schedule first arrival at t=0
+            FES.put((60, "chargingRate_change", -1))
+            chargers = Charger(NBSS)
+            while time < SIM_TIME:
+                (time, event_type, charger) = FES.get()
+                if time < data.oldT:
+                    print('Time travel')
+                if event_type == "arrival":
+                    arrival(time, FES, waitingLine)
+
+                elif event_type == "batteryAvailable":
+                    batteryAvailable(time, FES, waitingLine, charger)
+
+                elif event_type == "chargingRate_change":
+                    chargingRate_change(time, FES)
+
+                elif event_type == "reconnectBatteries":
+                    reconnectBatteries(time, FES)
+                config[key]['f'][f'Time_{f}'].append(time)
+                config[key]['f'][f'LossProb_{f}'].append(len(data.loss) / data.arr)
+                config[key]['f'][f'Cost_{f}'].append(data.cost)
+                config[key]['f'][f'Working_{f}'].append([charger.working for charger in chargers.chargers].count(True))
+    json_data = json.dumps(config, indent=4)
+    with open('data.json', 'w') as outfile:
+        outfile.write(json_data)
     # TODO compute the average waiting delay for k minus samples
